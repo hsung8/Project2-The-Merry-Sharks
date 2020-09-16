5579a52341a4704fb569039cf952aa74755c384f
Ethan Lam <ethanlam93@gmail.com>
2020-09-15 20:24:31 -0400

    update to fix Travis

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
-const db = require('./models');
+const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
